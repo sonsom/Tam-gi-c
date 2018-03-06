290dd8b45e87dec5f37d010d86947f5b2f35d3ad
xuanson <sondzuong3293@gmail.com>
2018-03-06 13:39:29 +0700

    tamgiacvuong

---
+++ tamgiacvuong.php
@@ -0,0 +1,17 @@
+<html>
+	<?php
+        for($i=0; $i <= 8; $i++)  
+		{  
+		   for ($j=1; $j <= $i; $j++)  
+			{  
+			 echo "*";  
+				if($j < $xi)  
+				 {  
+				   echo " ";  
+				 }  
+			 }  
+		   echo "<br>";  
+		}
+       ?>
+	
+</html>
