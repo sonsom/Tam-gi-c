033f07375fe318163d2e36cc7def83c590f3a7cf
xuanson <sondzuong3293@gmail.com>
2018-03-06 13:34:47 +0700

    tamgiaccan

---
+++ tamgiaccan.php
@@ -0,0 +1,23 @@
+<html>	
+<?php 
+$n=5;  
+		for($i=1; $i <= $n; $i++)  
+		{  
+		  for($j=1; $j <= $i; $j++)  
+		  {  
+		    echo ' * ';  
+		  }  
+		  echo '<br>';  
+		}  
+		for($i=$n; $i >= 1; $i--)  
+		{  
+		  for($j=1; $j <= $i; $j++)  
+		  {  
+		    echo ' * ';  
+		  }  
+		  echo '<br>';  
+		} 
+
+ ?>
+
+</html>
